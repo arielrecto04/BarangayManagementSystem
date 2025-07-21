7ce8b4bfb45d4a386dd40004921fb93e9e6b01e0
jlaurns <johnlaurence.junio@tup.edu.ph>
2025-07-21 17:56:47 +0800

    merge

--- resources/js/Pages/Resident/AddResident.vue
+++ resources/js/Pages/Resident/AddResident.vue
@@ -18,12 +18,12 @@
         </div>
 
         <!-- Second Row -->
-        <input type="date" placeholder="Birthday" class="input-style col-span-1 border border-gray-200 rounded-md px-4 py-2" />
+        <input type="text" placeholder="Birthday" class="border border-gray-200 rounded-md px-4 py-2" />
         <input type="text" placeholder="Age" class="input-style col-span-1 border border-gray-200 rounded-md px-4 py-2" />
         <input type="text" placeholder="Gender" class="input-style col-span-1 border border-gray-200 rounded-md px-4 py-2" />
 
         <!-- Third Row -->
-        <input type="text" placeholder="Lot no. / Street / Sub Division / Address" class="input-style col-span-2 border border-gray-200 rounded-md px-4 py-2" />
+        <input type="text" placeholder="Lot no. / Street / Subdivision / Barangay" class="input-style col-span-2 border border-gray-200 rounded-md px-4 py-2" />
         <button class="input-style bg-white  col-span-1 border border-gray-200 rounded-md px-4 py-2">Add Image</button>
 
         <!-- Fourth Row -->
@@ -32,7 +32,7 @@
         <input type="text" placeholder="Emergency Contact" class="input-style col-span-1 border border-gray-200 rounded-md px-4 py-2" />
       </div>
 
-      <!-- Action Buttons -->
+      
       <div class="flex justify-center mt-10 gap-4">
         <form action="" method="post" >
           <button type="submit" class="bg-green-500 hover:bg-green-600 text-white px-6 py-2 rounded-xl shadow-md">Save</button>
