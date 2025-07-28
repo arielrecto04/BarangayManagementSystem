a5fde91acd92f99104913246803b50a385380c66
JohnMacaraeg <johnmacaraeg4714@gmail.com>
2025-07-28 14:32:54 +0800

    listBlotter update

--- resources/js/Pages/Blotter/listBlotter.vue
+++ resources/js/Pages/Blotter/listBlotter.vue
@@ -1,49 +1,85 @@
 <script setup>
 import { AuthLayout } from "@/Layouts";
 import { Table } from '@/Components'
-import { useBlotterStore } from '@/Stores'
+import { useResidentStore } from '@/Stores'
 import { storeToRefs } from 'pinia';
 import { onMounted } from "vue";
 import useToast from '@/Utils/useToast';
 
 const { showToast } = useToast();
 
-const blotterStore = useBlotterStore();
-const { blotters, isLoading } = storeToRefs(blotterStore);
+const residentStore = useResidentStore();
+const { residents , isLoading } = storeToRefs(residentStore);
+
 
 const columns = [
-    { key: "blotter_no", label: "Blotter No" },
-    { key: "title_case", label: "Title Case" },
-    { key: "complainants", label: "Complainants" },
-    { key: "respondents", label: "Respondents" },
-    { key: "filing_date", label: "Filing Date" },
-];
+    {
+        key : "last_name" , label : "Last Name"
+    },
+    {
+        key : "first_name" , label : "First Name"
+    },
+    {
+        key : "birthday" , label : "Birthday"
+    },
+    {
+        key : "age" , label : "Age"
+    },
+    {
+        key : "gender" , label : "Gender"
+    },
+    {
+        key : "address" , label : "Address"
+    },
+    {
+        key : "contact_number" , label : "Contact Number"
+    },
+    {
+        key : "family_member" , label : "Family Member"
+    },
+    {
+        key : "emergency_contact" , label : "Emergency Contact"
+    },
+]
 
-const deleteBlotter = async (blotterId) => {
+
+
+const deleteResident = async (residentId) => {
     try {
-        await blotterStore.deleteBlotter(blotterId);
-        showToast({ icon: 'success', title: 'Blotter deleted successfully' });
+        await residentStore.deleteResident(residentId);
+        showToast({ icon: 'success', title: 'Resident deleted successfully' });
     } catch (error) {
         showToast({ icon: 'error', title: error.message });
     }
 }
 
+
 onMounted(() => {
-    blotterStore.getBlotters();
+    residentStore.getResidents();
 })
+
+
+
 </script>
 
 <template>
+
     <div class="flex flex-col gap-2">
-        <h1 class="text-xl font-bold text-gray-600">List of Blotters</h1>
+
+
+        <h1 class="text-xl font-bold text-gray-600">List of Residents</h1>
         <div v-if="isLoading" class="flex justify-center items-center">
             <div class="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
         </div>
-        <Table v-else :columns="columns" :rows="blotters">
-            <template #actions="{ row }">
-                <router-link :to="`/blotters/edit-blotter/${row.id}`" class="bg-blue-500 text-white px-2 py-1 rounded">Edit</router-link>
-                <button @click="deleteBlotter(row.id)" class="bg-red-500 text-white px-2 py-1 rounded">Delete</button>
+        <Table v-else :columns="columns" :rows="residents">
+
+            <template  #actions="{ row }">
+                <router-link :to="`/residents/edit-resident/${row.id}`" class="bg-blue-500 text-white px-2 py-1 rounded">Edit</router-link>
+                <button @click="deleteResident(row.id)" class="bg-red-500 text-white px-2 py-1 rounded">Delete</button>
             </template>
         </Table>
     </div>
+
+
+
 </template>
