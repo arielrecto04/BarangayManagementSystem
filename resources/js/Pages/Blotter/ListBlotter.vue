f5406d7336cfe438cf89d25c00681245ae558b37
jlaurns <johnlaurence.junio@tup.edu.ph>
2025-07-30 10:37:33 +0800

    ListBlotter

--- resources/js/Pages/Blotter/ListBlotter.vue
+++ resources/js/Pages/Blotter/ListBlotter.vue
@@ -24,11 +24,43 @@ const deleteBlotter = async (blotterId) => {
     try {
         await blotterStore.deleteBlotter(blotterId);
         showToast({ icon: 'success', title: 'Blotter deleted successfully' });
+>>>>>>> cd442b6 (ListBlotter)
     } catch (error) {
         showToast({ icon: 'error', title: error.message });
     }
 }
 
+<<<<<<< HEAD
+
+onMounted(() => {
+    residentStore.getResidents();
+})
+
+
+
+</script>
+
+<template>
+
+    <div class="flex flex-col gap-2">
+
+
+        <h1 class="text-xl font-bold text-gray-600">List of Residents</h1>
+        <div v-if="isLoading" class="flex justify-center items-center">
+            <div class="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+        </div>
+        <Table v-else :columns="columns" :rows="residents">
+
+            <template  #actions="{ row }">
+                <router-link :to="`/residents/edit-resident/${row.id}`" class="bg-blue-500 text-white px-2 py-1 rounded">Edit</router-link>
+                <button @click="deleteResident(row.id)" class="bg-red-500 text-white px-2 py-1 rounded">Delete</button>
+            </template>
+        </Table>
+    </div>
+
+
+
+=======
 onMounted(() => {
     blotterStore.getBlotters();
 })
@@ -49,4 +81,5 @@ onMounted(() => {
             </template>
         </Table>
     </div>
+>>>>>>> cd442b6 (ListBlotter)
 </template>
