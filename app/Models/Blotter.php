9efb3afa58092f148d4f31f5a6057dc88d388b7c
arielrecto04 <ariel.recto@innovatotec.com>
2025-08-07 17:43:03 +0800

    update:change blotter model

--- app/Models/Blotter.php
+++ app/Models/Blotter.php
@@ -42,7 +42,6 @@ class Blotter extends Model
         return $this->morphTo('respondent', 'respondents_type', 'respondents_id');
     }
 
-    protected $with = ['complainant', 'respondent'];
     public function getSupportingDocumentsAttribute($value)
     {
         return json_decode($value, true) ?: [];
@@ -53,15 +52,4 @@ class Blotter extends Model
         $this->attributes['supporting_documents'] = json_encode($value);
     }
 
-    protected $with = ['complainant', 'respondent'];
-    public function getSupportingDocumentsAttribute($value)
-    {
-        return json_decode($value, true) ?: [];
-    }
-
-    public function setSupportingDocumentsAttribute($value)
-    {
-        $this->attributes['supporting_documents'] = json_encode($value);
-    }
 }
-
